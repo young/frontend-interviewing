ce6e2dbf83c10fd44fa00f62e3ff74a7ba0b227f
Kunal <kunal.bohra@cancerlinq.org>
2019-10-30 12:47:59 -0700

    adding getByPath

---
+++ questions/getByPath.js
@@ -0,0 +1,10 @@
+// write a function that returns the value at a give path
+/*
+let testObj = {
+    d: 22,
+    m: 9,
+    o: {c: "MVD", i: "UY", f: {a: 56}}
+ };
+ 
+ getByPath(['o', 'f', 'a'], testObj);//56
+ */
