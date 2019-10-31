7c015333aa37d0d1fa2a54c6ec2f4d22108290c4
young <jem@netflix.com>
2019-10-30 17:56:06 -0700

    Updates

--- questions/duplicateStrings.js
+++ questions/duplicateStrings.js
@@ -1,4 +1,4 @@
 // Create a function that takes a string and returns a new string with duplicates removed
-//
-// const str = 'This is is a test string a';
+
+// const str = 'This is is a test test string';
 // removeDuplicates(str); // 'This is a test string'
--- questions/flatten.js
+++ questions/flatten.js
@@ -1,4 +1,4 @@
 // Without using .flat(), create a function to flatten an array
-const exampleArray = [1,2,[3,4, [5,6,7], 8], 9, 10];
 
+// const exampleArray = [1,2,[3,4, [5,6,7], 8], 9, 10];
 // flatten(exampleArray); // [1,2,3,4,5,6,7,8,9,10]
