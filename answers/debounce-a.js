1f427d83bde8d8c046fe53cfad6bfb8c25f899c2
Jem Young <jemuel.young@gmail.com>
2020-05-21 17:16:58 -0700

    Update debounce-a.js

--- answers/debounce-a.js
+++ answers/debounce-a.js
@@ -1,9 +1,14 @@
+/**
+ * Debounce invokes `fn()` once `time` has elapsed. The timer is reset if any debounce calls take place
+ * before time has elapsed.
+ */
 function debounce(fn, time) {
     let timeoutId;
 
     return function() {
       // Check for existing calls
       if (timeoutId) {
+        // Reset timer
         clearTimeout(timeoutId)
       }
 
