9c1900c691b5ef2782aae0ee830394f9a2f821f2
MalladiChaitanya <chaitanya.kmc@gmail.com>
2020-10-14 21:10:30 +0530

    Promisify in action

--- answers/advanced/promisify-a.js
+++ answers/advanced/promisify-a.js
@@ -1,11 +1,29 @@
 function promisify(fn) {
-    return function(...args) {
+    return function (...args) {
         return new Promise(function (resolve, reject) {
             function cb(result) {
                 resolve(result);
             }
 
-            fn.apply(this, args.concat(cb));
+            fn.apply(this, [cb].concat(args));
         });
     }
 }
+
+
+// Promisfy in action - using setTimeout
+let setTimeOutPromise = promisify(setTimeout);
+
+function printAndReturnPromise() {
+    console.log('First Callback function called');
+    return setTimeOutPromise(1000);
+}
+
+function printNameAfterGivenTime() {
+    console.log('Second Callback function called');
+}
+
+// Promise chain
+setTimeOutPromise(2000)
+    .then(printAndReturnPromise)
+    .then(printNameAfterGivenTime);
