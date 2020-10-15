5dd16c0da72cf27820eb91b5a71270af5706cc33
MalladiChaitanya <chaitanya.kmc@gmail.com>
2020-10-15 13:14:29 +0530

    Updated to match actual solution

--- answers/advanced/promisify-a.js
+++ answers/advanced/promisify-a.js
@@ -10,33 +10,23 @@ function promisify(fn) {
     }
 }
 
-/*
-function promisify(fn) {
-    return function (...args) {
-        return new Promise(function (resolve, reject) {
-            function cb(result) {
-                resolve(result);
-            }
-
-            fn.apply(this, [cb].concat(args));
-        });
-    }
+// Creating a custom setTimeout that takes callback at the end
+function customSetTimeout(...args) {
+    setTimeout(...args.reverse());
 }
-
-
-// Promisfy in action - using setTimeout
-let setTimeOutPromise = promisify(setTimeout);
+// Promisfy in action - using customSetTimeout
+let setTimeOutPromise = promisify(customSetTimeout);
 
 function printAndReturnPromise() {
     console.log('First Callback function called');
     return setTimeOutPromise(1000);
 }
 
-function printNameAfterGivenTime() {
+function printAfterGivenTime() {
     console.log('Second Callback function called');
 }
 
 // Promise chain
 setTimeOutPromise(2000)
     .then(printAndReturnPromise)
-    .then(printNameAfterGivenTime); */
+    .then(printAfterGivenTime);
