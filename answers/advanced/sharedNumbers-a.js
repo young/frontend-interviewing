b5ab8b24215dfa4244de983fba559fac783e8903
Vivek Nayyar <vivek.of.nayyar@gmail.com>
2021-06-25 03:36:04 +0200

    chore: changed from 2 to 4 spaces

--- answers/advanced/sharedNumbers-a.js
+++ answers/advanced/sharedNumbers-a.js
@@ -1,26 +1,26 @@
 function shared(x, y) {
-  const commonNumbers = [];
-  let curr1 = x.next();
-  let curr2 = y.next();
+    const commonNumbers = [];
+    let curr1 = x.next();
+    let curr2 = y.next();
 
-  while (curr1.done !== true && curr2.done !== true) {
-    if (curr1.value === curr2.value) {
-      commonNumbers.push(curr1.value);
-      curr1 = x.next();
-      curr2 = y.next();
-      continue;
+    while (curr1.done !== true && curr2.done !== true) {
+        if (curr1.value === curr2.value) {
+            commonNumbers.push(curr1.value);
+            curr1 = x.next();
+            curr2 = y.next();
+            continue;
+        }
+
+        if (curr1.value > curr2.value) {
+            curr2 = y.next();
+            continue;
+        }
+
+        if (curr1.value < curr2.value) {
+            curr1 = x.next();
+            continue;
+        }
     }
 
-    if (curr1.value > curr2.value) {
-      curr2 = y.next();
-      continue;
-    }
-
-    if (curr1.value < curr2.value) {
-      curr1 = x.next();
-      continue;
-    }
-  }
-
-  return commonNumbers;
+    return commonNumbers;
 }
